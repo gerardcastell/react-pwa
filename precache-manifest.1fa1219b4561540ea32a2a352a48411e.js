d1fae5f66405ff75091e5a2917ff737a6feb63fc
Gerard <gerardcastell97@gmail.com>
2019-11-22 18:16:05 +0100

    Updates

---
+++ precache-manifest.1fa1219b4561540ea32a2a352a48411e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9429f5e4c97cc3ae748292b818c63184",
+    "url": "/react-pwa/index.html"
+  },
+  {
+    "revision": "01ccf01b77531bea1273",
+    "url": "/react-pwa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "07b790b44169e5bc89e7",
+    "url": "/react-pwa/static/js/2.c2b5b8b1.chunk.js"
+  },
+  {
+    "revision": "01ccf01b77531bea1273",
+    "url": "/react-pwa/static/js/main.0497af47.chunk.js"
+  },
+  {
+    "revision": "a9025018d182926a698d",
+    "url": "/react-pwa/static/js/runtime-main.f69c1d25.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-pwa/static/media/logo.25bf045c.svg"
+  }
+]);
